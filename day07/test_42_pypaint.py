abba92320fbe2a44d64d9f4d17afceb421e6eccb
JW HWANG <kgwjddn544@gmail.com>
2024-02-20 17:24:10 +0900

    20240220 파이썬 실행파일

--- day07/test_42_pypaint.py
+++ day07/test_42_pypaint.py
@@ -16,8 +16,10 @@ class WinApp(QWidget):
         self.initSignal()
 
     def initUI(self): # 화면 초기화
-        uic.loadUi('./day07/pyPaint.ui',self)
-        self.setWindowIcon(QIcon('./images/iot.png'))
+        # uic.loadUi('./day07/pyPaint.ui',self)
+        uic.loadUi('C:/Sources/basic-python-2024/day07/pyPaint.ui',self)   # 실행 파일 생성시는 경로에 상대 경로가 없어져야함
+        # self.setWindowIcon(QIcon('./images/iot.png'))
+        self.setWindowIcon(QIcon('C:/Sources/basic-python-2024/day07/iot.png')) # 실행 파일 생성시는 경로에 상대 경로가 없어져야함
         self.setWindowTitle('Py그림판') 
         # 캔버스 초기화
         self.brushColor = Qt.black
